2ff8c2fad9101487de27dc72e9d1411e14c07163
fsworld <616749285@qq.com>
2024-02-29 11:29:32 +0800

    feat: 增加路由配置文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,5 +3,16 @@
  * @Date: 2024-02-28 16:38:32
  * @LastEditors: BGG
  * @LastEditTime: 2024-02-28 17:45:35
- * @Description: 
+ * @Description: 路由配置
  */
+
+import Home from './pages/home';
+import Detail from './pages/detail';
+
+const routers = [
+  {
+
+  }
+]
+
+export default routers
