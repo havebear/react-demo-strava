9aa766a87165e5377657414e87998daa1f2ab0f6
fsworld <616749285@qq.com>
2024-03-15 17:35:37 +0800

    feat: 集成 tailwind.css

--- src/pages/activities/index.tsx
+++ src/pages/activities/index.tsx
@@ -2,7 +2,7 @@
  * @Author: BGG
  * @Date: 2024-02-28 16:31:14
  * @LastEditors: BGG
- * @LastEditTime: 2024-03-15 15:43:24
+ * @LastEditTime: 2024-03-15 17:34:33
  * @Description: 活动
  */
 
@@ -11,7 +11,7 @@ import Hello from '../../components/Hello'
 
 const Activities: React.FC = () => {
   return (
-    <div>Active<Hello /></div>
+    <div className='text-shadow sticky top-0 z-10 mb-4 text-lg font-bold text-gray-700 dark:text-white dark:text-opacity-70'>Active<Hello /></div>
   )
 };
 
