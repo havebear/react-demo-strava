ab52edee49a3686c1e2de36a2a428f5decef7a92
fsworld <616749285@qq.com>
2024-09-13 15:32:12 +0800

    feat: 备份顶部菜单搜索栏代码

--- src/pages/home/components/SearchBar.tsx
+++ src/pages/home/components/SearchBar.tsx
@@ -2,17 +2,49 @@
  * @Author: BGG
  * @Date: 2024-09-11 14:30:43
  * @LastEditors: BGG
- * @LastEditTime: 2024-09-12 15:55:00
+ * @LastEditTime: 2024-09-13 15:00:54
  * @Description: 搜索栏
  */
 
 import React from 'react'
 import style from '../css/SearchBar.module.less'
 
+const SEATCH_ICON = '/images/icon_search.png'
+const CLOSE_ICON = '/images/icon_close.png'
+
+
 const SearchBar: React.FC = () => {
+
+  const [status, setStatus] = React.useState(false)
+
+  // 切换状态，控制搜索框显示隐藏
+  const switchStatus = () => {
+    setStatus(!status)
+  }
+
+  // 正常状态
+  const normalEl = (
+    <img
+      className={style.search_icon}
+      src={SEATCH_ICON}
+      onClick={switchStatus} />
+  )
+
+  // 搜索状态
+  const activeEl = (
+    <div className={style.search_active}>
+      <div>Select</div>
+      <div className={style.search_input_wrap}>
+        <input className={style.search_input} placeholder="搜索" />
+        <img className={`${style.search_icon} ${style.search_btn}`} src={SEATCH_ICON} onClick={switchStatus} />
+      </div>
+      <img className={`${style.search_icon} ${style.search_close}`} src={CLOSE_ICON} onClick={switchStatus} />
+    </div>
+  )
+
   return (
     <div className={style.search}>
-      <img className={style.search_icon} src="/images/icon_search.png" />
+      { status ? activeEl : normalEl }
     </div>
   )
 }
